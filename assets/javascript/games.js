ee10c29b287709bd5dd4c8f5f29c15a64dcb626a
Osy odife <osyodife@gmail.com>
2019-06-16 21:09:06 -0400

    upload modified files

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,12 +1,2 @@
 
-var numTrialsRemaining=0;
-function numTrials() {
-if(numTrialsRemaining==0){
-return numTrialsRemaining=10;
-document.getElementById("guessleft").innerHTML = numTrials();
 
-}else{
-return numTrialsRemaining+=0;
-document.getElementById("guessleft").innerHTML = numTrials();
-}
-}
